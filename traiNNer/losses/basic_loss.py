b7d10e6e9d7c2593f3b1ee4a0eb5796211505a2d
umzi <ab5447612@gmail.com>
2024-06-01 23:17:01 +0300

    neosr perceptual loss

--- traiNNer/losses/basic_loss.py
+++ traiNNer/losses/basic_loss.py
@@ -146,7 +146,7 @@ class WeightedTVLoss(L1Loss):
 
 @LOSS_REGISTRY.register()
 class PerceptualLoss(nn.Module):
-    """Perceptual loss with commonly used style loss.
+    """Perceptual loss with VGG19
 
     Args:
         layer_weights (dict): The weight for each layer of vgg feature.
@@ -162,41 +162,45 @@ class PerceptualLoss(nn.Module):
         perceptual_weight (float): If `perceptual_weight > 0`, the perceptual
             loss will be calculated and the loss will multiplied by the
             weight. Default: 1.0.
-        style_weight (float): If `style_weight > 0`, the style loss will be
-            calculated and the loss will multiplied by the weight.
-            Default: 0.
-        criterion (str): Criterion used for perceptual loss. Default: 'l1'.
+        criterion (str): Criterion used for perceptual loss. Default: 'huber'.
     """
 
-    def __init__(self,
-                 layer_weights,
-                 vgg_type='vgg19',
-                 use_input_norm=True,
-                 range_norm=False,
-                 perceptual_weight=1.0,
-                 style_weight=0.,
-                 criterion='l1'):
+    def __init__(
+        self,
+        layer_weights,
+        vgg_type: str = "vgg19",
+        use_input_norm: bool = True,
+        range_norm: bool = False,
+        perceptual_weight: float = 1.0,
+        criterion: str = "huber",
+        **kwargs,
+    ) -> None:
         super(PerceptualLoss, self).__init__()
         self.perceptual_weight = perceptual_weight
-        self.style_weight = style_weight
         self.layer_weights = layer_weights
+
         self.vgg = VGGFeatureExtractor(
             layer_name_list=list(layer_weights.keys()),
             vgg_type=vgg_type,
             use_input_norm=use_input_norm,
-            range_norm=range_norm)
+            range_norm=range_norm,
+        )
 
         self.criterion_type = criterion
-        if self.criterion_type == 'l1':
-            self.criterion = torch.nn.L1Loss()
-        elif self.criterion_type == 'l2':
-            self.criterion = torch.nn.MSELoss()
-        elif self.criterion_type == 'fro':
+        if self.criterion_type == "l1":
+            self.criterion = nn.L1Loss()
+        elif self.criterion_type == "l2":
+            self.criterion = nn.MSELoss()
+        elif self.criterion_type == "huber":
+            self.criterion = nn.HuberLoss()
+        elif self.criterion_type == "fro":
             self.criterion = None
         else:
-            raise NotImplementedError(f'{criterion} criterion has not been supported.')
+            raise NotImplementedError(f"{criterion} criterion not supported.")
 
-    def forward(self, x, gt):
+    def forward(
+        self, x: torch.Tensor, gt: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor]:
         """Forward function.
 
         Args:
@@ -214,29 +218,21 @@ class PerceptualLoss(nn.Module):
         if self.perceptual_weight > 0:
             percep_loss = 0
             for k in x_features.keys():
-                if self.criterion_type == 'fro':
-                    percep_loss += torch.norm(x_features[k] - gt_features[k], p='fro') * self.layer_weights[k]
+                if self.criterion_type == "fro":
+                    # note: linalg.norm uses Frobenius norm by default
+                    percep_loss += (
+                        torch.linalg.norm(x_features[k] - gt_features[k])
+                        * self.layer_weights[k]
+                    )
                 else:
-                    percep_loss += self.criterion(x_features[k], gt_features[k]) * self.layer_weights[k]
+                    percep_loss += (
+                        self.criterion(x_features[k], gt_features[k])
+                        * self.layer_weights[k]
+                    )
+
             percep_loss *= self.perceptual_weight
-        else:
-            percep_loss = None
 
-        # calculate style loss
-        if self.style_weight > 0:
-            style_loss = 0
-            for k in x_features.keys():
-                if self.criterion_type == 'fro':
-                    style_loss += torch.norm(
-                        self._gram_mat(x_features[k]) - self._gram_mat(gt_features[k]), p='fro') * self.layer_weights[k]
-                else:
-                    style_loss += self.criterion(self._gram_mat(x_features[k]), self._gram_mat(
-                        gt_features[k])) * self.layer_weights[k]
-            style_loss *= self.style_weight
-        else:
-            style_loss = None
-
-        return percep_loss, style_loss
+        return percep_loss
 
     def _gram_mat(self, x):
         """Calculate Gram matrix.
